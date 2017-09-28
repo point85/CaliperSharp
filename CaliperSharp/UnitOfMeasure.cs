62e30a212b176dcb864b245551a8c2b0aa82ffdf
billran <kentra@pacbell.net>
2017-09-27 19:32:26 -0700

    wip

--- CaliperSharp/UnitOfMeasure.cs
+++ CaliperSharp/UnitOfMeasure.cs
@@ -86,7 +86,7 @@ namespace CaliperSharp
 		};
 
 		// maximum length of the symbol
-		private readonly int MAX_SYMBOL_LENGTH = 16;
+		private const int MAX_SYMBOL_LENGTH = 16;
 
 		// multiply, divide and power symbols
 		internal static readonly char MULT = (char)0xB7;
@@ -233,16 +233,6 @@ namespace CaliperSharp
 		}
 
 		/**
-		* Get the exponent
-		* 
-		* @return Exponent
-*/
-		public Nullable<int> GetPowerExponent()
-		{
-			return Exponent1;
-		}
-
-		/**
  * Check to see if this unit of measure has a conversion to another unit of
  * measure other than itself.
  * 
@@ -253,6 +243,93 @@ namespace CaliperSharp
 			return this.Equals(AbscissaUnit) ? true : false;
 		}
 
+		/**
+ * Get the exponent
+ * 
+ * @return Exponent
+ */
+		public int? GetPowerExponent()
+		{
+			return Exponent1;
+		}
+
+		/**
+ * Get the dividend unit of measure
+ * 
+ * @return {@link UnitOfMeasure}
+ */
+		public UnitOfMeasure GetDividend()
+		{
+			return UOM1;
+		}
+
+		/**
+		 * Get the divisor unit of measure
+		 * 
+		 * @return {@link UnitOfMeasure}
+		 */
+		public UnitOfMeasure GetDivisor()
+		{
+			return UOM2;
+		}
+
+		/**
+ * Set the base unit of measure and exponent
+ * 
+ * @param base
+ *            Base unit of measure
+ * @param exponent
+ *            Exponent
+ * @throws Exception
+ *             Exception
+ */
+		public void SetPowerUnit(UnitOfMeasure baseUOM, int exponent)
+		{
+			if (baseUOM == null)
+			{
+				String msg = String.Format(MeasurementSystem.GetMessage("base.cannot.be.null"), Symbol);
+				throw new Exception(msg);
+			}
+			SetPowerProduct(baseUOM, exponent);
+		}
+
+		// generate a symbol for units of measure created as the result of
+		// intermediate multiplication and division operations. These symbols are
+		// not cached.
+		private static string GenerateIntermediateSymbol()
+		{
+			return Environment.TickCount.ToString("X");
+		}
+
+		internal static String GeneratePowerSymbol(UnitOfMeasure baseUOM, int exponent)
+		{
+			StringBuilder sb = new StringBuilder();
+			sb.Append(baseUOM.Symbol).Append(POW).Append(exponent);
+			return sb.ToString();
+		}
+
+		internal static string GenerateProductSymbol(UnitOfMeasure multiplier, UnitOfMeasure multiplicand)
+		{
+			StringBuilder sb = new StringBuilder();
+
+			if (multiplier.Equals(multiplicand))
+			{
+				sb.Append(multiplier.Symbol).Append(SQ);
+			}
+			else
+			{
+				sb.Append(multiplier.Symbol).Append(MULT).Append(multiplicand.Symbol);
+			}
+			return sb.ToString();
+		}
+
+		internal static string GenerateQuotientSymbol(UnitOfMeasure dividend, UnitOfMeasure divisor)
+		{
+			StringBuilder sb = new StringBuilder();
+			sb.Append(dividend.Symbol).Append(DIV).Append(divisor.Symbol);
+			return sb.ToString();
+		}
+
 		UnitOfMeasure ClonePower(UnitOfMeasure uom)
 		{
 
@@ -260,28 +337,29 @@ namespace CaliperSharp
 			newUOM.UnitType = UnitType;
 
 			// check if quotient
-			int? exponent = 1;
+			int exponent = 1;
 			if (GetPowerExponent() != null)
 			{
-				exponent = GetPowerExponent();
+				exponent = GetPowerExponent().Value;
 			}
 
 			UnitOfMeasure one = MeasurementSystem.GetSystem().GetOne();
 			if (GetMeasurementType().Equals(MeasurementType.QUOTIENT))
 			{
-				if (GetDividend().equals(one))
+				if (GetDividend().Equals(one))
 				{
-					exponent = getExponent2();
+					exponent = Exponent2.Value;
 				}
-				else if (getDivisor().equals(one))
+				else if (GetDivisor().Equals(one))
 				{
-					exponent = getExponent1();
+					exponent = Exponent1.Value;
 				}
 			}
-			newUOM.setPowerUnit(uom, exponent);
-			String symbol = UnitOfMeasure.generatePowerSymbol(uom, exponent);
-			newUOM.setSymbol(symbol);
-			newUOM.setName(symbol);
+
+			newUOM.SetPowerUnit(uom, exponent);
+			String symbol = UnitOfMeasure.GeneratePowerSymbol(uom, exponent);
+			newUOM.Symbol = symbol;
+			newUOM.Name = symbol;
 
 			return newUOM;
 		}
@@ -297,7 +375,7 @@ namespace CaliperSharp
 			}
 			else
 			{
-				value = decimal.Multiply(a,b);
+				value = decimal.Multiply(a, b);
 			}
 			return value;
 		}
@@ -358,6 +436,317 @@ namespace CaliperSharp
 			return Symbol.CompareTo(other.Symbol);
 		}
 
+		// reduce a unit of measure to its most basic scalar units of measure.
+		private class Reducer
+		{
+			private static readonly int MAX_RECURSIONS = 100;
+
+			// starting level
+			private static readonly int STARTING_LEVEL = -1;
+
+			// UOMs and their exponents
+			private Dictionary<UnitOfMeasure, int> Terms { get; set; } = new Dictionary<UnitOfMeasure, int>();
+
+			// the overall scaling factor
+			private decimal MapScalingFactor { get; set; } = decimal.One;
+
+			// list of exponents down a path to the leaf UOM
+			private List<int> PathExponents = new List<int>();
+
+			// recursion counter
+			private int Counter = 0;
+
+			internal Reducer()
+			{
+
+			}
+
+			internal void Explode(UnitOfMeasure unit)
+			{
+				ExplodeRecursively(unit, STARTING_LEVEL);
+			}
+
+			private void ExplodeRecursively(UnitOfMeasure unit, int level)
+			{
+				if (++Counter > MAX_RECURSIONS)
+				{
+					string msg = String.Format(MeasurementSystem.GetMessage("circular.references"),
+							unit.Symbol);
+					throw new Exception(msg);
+				}
+
+				// down a level
+				level++;
+
+				// scaling factor to abscissa unit
+				decimal scalingFactor = unit.ScalingFactor;
+
+				// explode the abscissa unit
+				UnitOfMeasure abscissaUnit = unit.AbscissaUnit;
+
+				UnitOfMeasure uom1 = abscissaUnit.UOM1;
+				UnitOfMeasure uom2 = abscissaUnit.UOM2;
+
+				int? exp1 = abscissaUnit.Exponent1;
+				int? exp2 = abscissaUnit.Exponent2;
+
+				// scaling
+				if (PathExponents.Count > 0)
+				{
+					int lastExponent = PathExponents[PathExponents.Count - 1];
+
+					// compute the overall scaling factor
+					decimal factor = decimal.One;
+					for (int i = 0; i < Math.Abs(lastExponent); i++)
+					{
+						factor = UnitOfMeasure.DecimalMultiply(factor, scalingFactor);
+					}
+
+					if (lastExponent < 0)
+					{
+						MapScalingFactor = UnitOfMeasure.DecimalDivide(MapScalingFactor, factor);
+					}
+					else
+					{
+						MapScalingFactor = UnitOfMeasure.DecimalMultiply(MapScalingFactor, factor);
+					}
+				}
+				else
+				{
+					MapScalingFactor = scalingFactor;
+				}
+
+				if (uom1 == null)
+				{
+					if (!abscissaUnit.IsTerminal())
+					{
+						// keep exploding down the conversion path
+						decimal currentMapFactor = MapScalingFactor;
+						MapScalingFactor = decimal.One;
+						ExplodeRecursively(abscissaUnit, STARTING_LEVEL);
+						MapScalingFactor = UnitOfMeasure.DecimalMultiply(MapScalingFactor, currentMapFactor);
+					}
+					else
+					{
+
+						// multiply out all of the exponents down the path
+						int pathExponent = 1;
+
+						foreach (int exp in PathExponents)
+						{
+							pathExponent = pathExponent * exp;
+						}
+
+						bool invert = pathExponent < 0 ? true : false;
+
+						for (int i = 0; i < Math.Abs(pathExponent); i++)
+						{
+							AddTerm(abscissaUnit, invert);
+						}
+					}
+				}
+				else
+				{
+					// explode UOM #1
+					PathExponents.Add(exp1.Value);
+					ExplodeRecursively(uom1, level);
+					PathExponents.Remove(level);
+				}
+
+				if (uom2 != null)
+				{
+					// explode UOM #2
+					PathExponents.Add(exp2.Value);
+					ExplodeRecursively(uom2, level);
+					PathExponents.Remove(level);
+				}
+
+				// up a level
+				level--;
+			}
+
+			// add a UOM and exponent pair to the map of reduced terms
+			private void AddTerm(UnitOfMeasure uom, bool invert)
+			{
+				int unitPower = 1;
+				int power = 0;
+
+				if (!invert)
+				{
+					// get existing power
+					if (!Terms.ContainsKey(uom))
+					{
+						// add first time
+						power = unitPower;
+					}
+					else
+					{
+						// increment existing power
+						if (!uom.Equals(MeasurementSystem.GetSystem().GetOne()))
+						{
+							power = Terms[uom] + unitPower;
+						}
+					}
+				}
+				else
+				{
+					// denominator with negative powers
+					if (!Terms.ContainsKey(uom))
+					{
+						// add first time
+						power = -unitPower;
+					}
+					else
+					{
+						// decrement existing power
+						if (!uom.Equals(MeasurementSystem.GetSystem().GetOne()))
+						{
+							power = Terms[uom] - unitPower;
+						}
+					}
+				}
+
+				if (power == 0)
+				{
+					Terms.Remove(uom);
+				}
+				else
+				{
+					if (!uom.Equals(MeasurementSystem.GetSystem().GetOne()))
+					{
+						Terms[uom] = power;
+					}
+				}
+			}
+
+			// compose the base symbol
+			internal string BuildBaseString()
+			{
+				StringBuilder numerator = new StringBuilder();
+				StringBuilder denominator = new StringBuilder();
+
+				int numeratorCount = 0;
+				int denominatorCount = 0;
+
+				// sort units by symbol (ascending)
+				SortedSet<UnitOfMeasure> keys = new SortedSet<UnitOfMeasure>(Terms.Keys);
+
+				foreach (UnitOfMeasure unit in keys)
+				{
+					int power = Terms[unit];
+
+					if (power < 0)
+					{
+						// negative, put in denominator
+						if (denominator.Length > 0)
+						{
+							denominator.Append(UnitOfMeasure.MULT);
+						}
+
+						if (!unit.Equals(MeasurementSystem.GetSystem().GetOne()))
+						{
+							denominator.Append(unit.Symbol);
+							denominatorCount++;
+						}
+
+						if (power < -1)
+						{
+							if (power == -2)
+							{
+								denominator.Append(UnitOfMeasure.SQ);
+							}
+							else if (power == -3)
+							{
+								denominator.Append(UnitOfMeasure.CUBED);
+							}
+							else
+							{
+								denominator.Append(UnitOfMeasure.POW).Append(Math.Abs(power));
+							}
+						}
+					}
+					else if (power >= 1 && !unit.Equals(MeasurementSystem.GetSystem().GetOne()))
+					{
+						// positive, put in numerator
+						if (numerator.Length > 0)
+						{
+							numerator.Append(UnitOfMeasure.MULT);
+						}
+
+						numerator.Append(unit.Symbol);
+						numeratorCount++;
+
+						if (power > 1)
+						{
+							if (power == 2)
+							{
+								numerator.Append(UnitOfMeasure.SQ);
+							}
+							else if (power == 3)
+							{
+								numerator.Append(UnitOfMeasure.CUBED);
+							}
+							else
+							{
+								numerator.Append(UnitOfMeasure.POW).Append(power);
+							}
+						}
+					}
+					else
+					{
+						// unary, don't add a '1'
+					}
+				}
+
+				if (numeratorCount == 0)
+				{
+					numerator.Append(UnitOfMeasure.ONE_CHAR);
+				}
+
+				string result = null;
+
+				if (denominatorCount == 0)
+				{
+					result = numerator.ToString();
+				}
+				else
+				{
+					if (denominatorCount == 1)
+					{
+						result = numerator.Append(UnitOfMeasure.DIV).Append(denominator).ToString();
+					}
+					else
+					{
+						result = numerator.Append(UnitOfMeasure.DIV).Append(UnitOfMeasure.LP).Append(denominator).Append(UnitOfMeasure.RP).ToString();
+					}
+				}
+
+				return result;
+			} // end unit of measure iteration
+		} // end Reducer class
+
+		// UOM, scaling factor and power cumulative along a conversion path
+		private class PathParameters
+		{
+			private UnitOfMeasure PathUOM;
+			private decimal PathFactor;
+
+			private PathParameters(UnitOfMeasure pathUOM, decimal pathFactor)
+			{
+				PathUOM = pathUOM;
+				PathFactor = pathFactor;
+			}
+
+			private UnitOfMeasure GetPathUOM()
+			{
+				return PathUOM;
+			}
+
+			private decimal GetPathFactor()
+			{
+				return PathFactor;
+			}
+		}
 
 	} // end UnitOfMeasure class
 } // end namespace
