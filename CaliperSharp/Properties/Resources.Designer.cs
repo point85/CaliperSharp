dc72c87d9073864e1cad8184f4eb7849b818a01e
billran <kentra@pacbell.net>
2017-10-12 16:27:01 -0700

    Added a release dll

--- CaliperSharp/Properties/Resources.Designer.cs
+++ CaliperSharp/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace org.point85.uom.Properties {
+namespace Point85.Caliper.UnitOfMeasure.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace org.point85.uom.Properties {
         public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("org.point85.uom.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Point85.Caliper.UnitOfMeasure.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
